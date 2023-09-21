fd17bbf34370e9dfebf83369322316ce5d7e6775
Guido <ggordyn@itba.edu.ar>
2023-09-21 14:30:37 -0300

    3rd person movement
    Co-Authored-By: Ana Tarantino <atarantino@itba.edu.ar>
    Co-Authored-By: Martina Rodriguez <58373738+martirodriguez98@users.noreply.github.com>

--- Assets/Scripts/Player/PlayerMovementController.cs
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -2,14 +2,16 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour, IMoveable
+public class PlayerMovementController : MonoBehaviour, IMoveable
 {
     public float Speed => _speed;
-    private float _speed = 10;
+    private float _speed = 6f;
+
+    float turnSmoothVelocity;
     
-    
-    public void Move(Vector3 direction){
-        
+    public void Move(Vector3 direction)
+    {
+        transform.position += direction * Time.deltaTime * Speed;
     }
 
 }
