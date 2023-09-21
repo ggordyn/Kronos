053fb032571004922feae01184159d9dfaf2325a
Guido <ggordyn@itba.edu.ar>
2023-09-21 13:51:01 -0300

    Initial commit

---
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour, IMoveable
+{
+    public float Speed => _speed;
+    private float _speed = 10;
+    
+    public void (Vector3 direction){
+        
+    }
+
+}
